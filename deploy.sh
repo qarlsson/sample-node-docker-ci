ea4c716047260c7241b22eaeffb2b0e53834b357
Alexey Kosterin <a@kosterine.ru>
2017-03-07 11:42:45 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ssh a@130.211.74.89 << EOF
-docker pull kostyaurysov/sample-node:latest
+docker pull qarlsson/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi qarlsson/sample-node:current || true
+docker tag qarlsson/sample-node:latest qarlsson/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 qarlsson/sample-node:current
 EOF
