f052dcf9294b6fe74d900c2f70eabd2598a6898b
Alexey Kosterin <a@kosterine.ru>
2017-03-20 07:35:16 +0000

    fixed

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ssh a@130.211.74.89 << EOF
+ssh -i key alekskosterin@130.211.89.73  << EOF
 docker pull qarlsson/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi qarlsson/sample-node:current || true
 docker tag qarlsson/sample-node:latest qarlsson/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 qarlsson/sample-node:current
+docker run -d --net app --name web --restart always  -p 80:80 qarlsson/sample-node:current
 EOF
